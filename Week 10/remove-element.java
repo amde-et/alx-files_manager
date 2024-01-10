17a4da7ddb169e4e64c94188b8cecab6b7907e1c
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2024-01-10 16:19:51 +0300

    Added solution for Remove Element

---
+++ Week 10/remove-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j=0;
+        while(j < nums.length)
+        {
+            if(nums[j] != val)
+            {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
